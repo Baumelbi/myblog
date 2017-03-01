e67680d06b8e8f2af907adaf5606f33a7ab18c0a
Brad Baumel <Baumelbi@gmail.com>
2017-02-28 19:53:41 -0800

    'setting up the views'

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import RequestContext, loader
+from myblog.models import Post
+
+
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('list.html')
+    context = RequestContext(request, {
+        'posts': posts,
+    })
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
